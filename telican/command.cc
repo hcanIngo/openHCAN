25277531e4908edf05d4a1616aee08d06b00ab02
ingo <il@iEN>
2013-05-27 18:42:20 +0200

    gitignore *so*

--- telican/command.cc
+++
@@ -1,5 +0,0 @@
-#include "command.h"
-
-command::~command()
-{
-}
--- telican/readline_completion_generator.cc
+++
@@ -1,7 +0,0 @@
-#include "readline_completion_generator.h"
-
-using namespace readlinepp;
-
-completion_generator::~completion_generator()
-{
-}

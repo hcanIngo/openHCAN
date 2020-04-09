b94bcdda070d41e049f6231cdb243c4f4ba1a1b8
Martin <RealMerlin@users.noreply.github.com>
2020-04-09 22:05:09 +0200

    Telican Fehlerhandling verbessert

--- telican/heizung_command.cc
+++ telican/heizung_command.cc
@@ -17,56 +17,70 @@ heizung_command::~heizung_command()
 
 float heizung_command::get_ist_temp(uint8_t id)
 {
-	uint8_t dummy,hi,lo;
-	m_tcon.send_HEIZUNG_TIST_REQUEST(m_src, HCAN_MULTICAST_CONTROL,id);
-	m_tcon.recv_HEIZUNG_TIST_REPLAY(0,m_src,&dummy,&hi,&lo);
-	return ((hi << 8) | lo) / 16.0;
+	try
+	{
+		uint8_t dummy,hi,lo;
+		m_tcon.send_HEIZUNG_TIST_REQUEST(m_src, HCAN_MULTICAST_CONTROL,id);
+		m_tcon.recv_HEIZUNG_TIST_REPLAY(0,m_src,&dummy,&hi,&lo);
+		return ((hi << 8) | lo) / 16.0;
+	}
+	catch (traceable_error &e)
+	{
+		throw e;//cerr << e.what() << endl;
+	}
 }
 
 heizung_info_t heizung_command::get_details(uint8_t id)
 {
-	heizung_info_t info;
-	m_tcon.send_HEIZUNG_DETAILS_REQUEST(m_src, HCAN_MULTICAST_CONTROL,id);
-
-	hcan::frame f = m_tcon.recv_frame(m_src);
-
-	switch (f.data(1))
+	try
 	{
-		case HCAN_HES_HEIZUNG_MODE_OFF_DETAILS :
-			info.mode = 0;
-			break;
-		case HCAN_HES_HEIZUNG_MODE_MANUAL_DETAILS :
-			info.mode = 1;
-			info.stellgrad = f.data(3);
-			info.dauer = (f.data(4)<<8 | f.data(5));
-			info.dauer_min = info.dauer / 60;
-			info.dauer_sec = info.dauer % 60;
-			break;
-		case HCAN_HES_HEIZUNG_MODE_THERMOSTAT_DETAILS :
-			info.mode = 2;
-			info.soll_temp = ((f.data(3)<<8)|f.data(4))/16.0;
-			info.ist_temp = get_ist_temp(id);
-			info.dauer = (f.data(5)<<8) | f.data(6);
-			info.dauer_min = info.dauer / 60;
-			info.dauer_sec = info.dauer % 60;
-			break;
-		case HCAN_HES_HEIZUNG_MODE_AUTOMATIK_DETAILS :
-			info.mode = 3;
-			info.soll_temp = (f.data(3)<<8|f.data(4))/16.0;
-			info.zz_id = f.data(5);
-			break;
-		default:
-			throw traceable_error("unknown heizung mode");
+		heizung_info_t info;
+		m_tcon.send_HEIZUNG_DETAILS_REQUEST(m_src, HCAN_MULTICAST_CONTROL,id);
+
+		hcan::frame f = m_tcon.recv_frame(m_src);
+
+		switch (f.data(1))
+		{
+			case HCAN_HES_HEIZUNG_MODE_OFF_DETAILS :
+				info.mode = 0;
+				break;
+			case HCAN_HES_HEIZUNG_MODE_MANUAL_DETAILS :
+				info.mode = 1;
+				info.stellgrad = f.data(3);
+				info.dauer = (f.data(4)<<8 | f.data(5));
+				info.dauer_min = info.dauer / 60;
+				info.dauer_sec = info.dauer % 60;
+				break;
+			case HCAN_HES_HEIZUNG_MODE_THERMOSTAT_DETAILS :
+				info.mode = 2;
+				info.soll_temp = ((f.data(3)<<8)|f.data(4))/16.0;
+				info.ist_temp = get_ist_temp(id);
+				info.dauer = (f.data(5)<<8) | f.data(6);
+				info.dauer_min = info.dauer / 60;
+				info.dauer_sec = info.dauer % 60;
+				break;
+			case HCAN_HES_HEIZUNG_MODE_AUTOMATIK_DETAILS :
+				info.mode = 3;
+				info.soll_temp = (f.data(3)<<8|f.data(4))/16.0;
+				info.zz_id = f.data(5);
+				break;
+			default:
+				throw traceable_error("unknown heizung mode");
+		}
+
+		info.ist_temp = get_ist_temp(id);
+
+		stringstream ss;
+		ss << setw(3) << info.dauer_min << ":" << setw(2) << setfill('0')
+			<< info.dauer_sec << "min";
+		info.dauer_str = ss.str();
+
+		return info;
 	}
-
-	info.ist_temp = get_ist_temp(id);
-
-	stringstream ss;
-	ss << setw(3) << info.dauer_min << ":" << setw(2) << setfill('0') 
-		<< info.dauer_sec << "min";
-	info.dauer_str = ss.str();
-
-	return info;
+	catch (traceable_error &e)
+	{
+		throw e;//cerr << e.what() << endl;
+	}
 }
 
 void heizung_command::print_header()
@@ -80,12 +94,21 @@ void heizung_command::print_header()
 void heizung_command::print_heizung(uint8_t id)
 {
 	cout << "| " << setw(3) << uint16_t(id) << " | ";
-	heizung_info_t info = get_details(id);
+	heizung_info_t info = {};
+	try
+	{
+		info = get_details(id);
+	}
+	catch (traceable_error &e)
+	{
+		cerr << e.what() << endl;
+		return;
+	}
 
 	switch (info.mode)
 	{
 		case HEIZUNG_MODE_OFF :
-			cout << "aus     | " 
+			cout << "aus     | "
 				<< setiosflags(ios::fixed) << setprecision(1) << setw(4)
 				<< info.ist_temp
 				<< "   |         |       |           | ";
@@ -99,7 +122,7 @@ void heizung_command::print_heizung(uint8_t id)
 				<< info.dauer_str << " | ";
 			break;
 		case HEIZUNG_MODE_THERMOSTAT :
-			cout << "therm.  | " 
+			cout << "therm.  | "
 				<< setiosflags(ios::fixed) << setprecision(1) 
 				<< setfill('0') << setw(4)
 				<< info.ist_temp << "   | "
@@ -107,7 +130,7 @@ void heizung_command::print_heizung(uint8_t id)
 			   	<< info.dauer_str << " | ";
 			break;
 		case HEIZUNG_MODE_AUTO :
-			cout << "auto    | " 
+			cout << "auto    | "
 				<< setiosflags(ios::fixed) << setprecision(1) << setw(4)
 				<< setfill('0')
 				<< info.ist_temp << "   | "
@@ -138,68 +161,75 @@ void heizung_command::print_heizungen()
 
 bool heizung_command::exec(const string &line)
 {
-	boost::cmatch matches;
-
-	boost::regex re("^heizung *$");
-	if (boost::regex_match(line.c_str(), matches, re))
+	try
 	{
-		print_heizungen();
-		return true;
+		boost::cmatch matches;
+
+		boost::regex re("^heizung *$");
+		if (boost::regex_match(line.c_str(), matches, re))
+		{
+			print_heizungen();
+			return true;
+		}
+
+		boost::regex re2("^heizung *([[:word:]-]+) *$");
+		if (boost::regex_match(line.c_str(), matches, re2))
+		{
+			print_header();
+			print_heizung(global_installation_data.heizung_id_by_name(
+						matches[1]));
+			return true;
+		}
+
+		boost::regex re3("^heizung *([[:word:]-]+) +auto *$");
+		if (boost::regex_match(line.c_str(), matches, re3))
+		{
+			uint8_t id = global_installation_data.heizung_id_by_name(
+					matches[1]);
+			m_tcon.send_HEIZUNG_SET_MODE_AUTOMATIK(m_src,
+					HCAN_MULTICAST_CONTROL,id);
+			return true;
+		}
+
+		boost::regex re4("^heizung *([[:word:]-]+) +aus *$");
+		if (boost::regex_match(line.c_str(), matches, re4))
+		{
+			uint8_t id = global_installation_data.heizung_id_by_name(
+					matches[1]);
+			m_tcon.send_HEIZUNG_SET_MODE_OFF(m_src,
+					HCAN_MULTICAST_CONTROL,id);
+			return true;
+		}
+
+		boost::regex re5("^heizung *([[:word:]-]+) +manuell +([0-9]+) +([0-9]+) *$");
+		if (boost::regex_match(line.c_str(), matches, re5))
+		{
+			uint8_t id = global_installation_data.heizung_id_by_name(
+					matches[1]);
+			uint8_t stellgrad = boost::lexical_cast<int>(matches[2]);
+			uint16_t dauer = boost::lexical_cast<int>(matches[3]) * 60; // Sekunden!
+			m_tcon.send_HEIZUNG_SET_MODE_MANUAL(m_src,
+					HCAN_MULTICAST_CONTROL,id,stellgrad,dauer>>8,dauer);
+			return true;
+		}
+
+		boost::regex re6("^heizung *([[:word:]-]+) +therm +([0-9\\.]+) +([0-9]+) *$");
+		if (boost::regex_match(line.c_str(), matches, re6))
+		{
+			uint8_t id = global_installation_data.heizung_id_by_name(
+					matches[1]);
+			float temp = boost::lexical_cast<float>(matches[2]);
+			uint8_t hi = (uint16_t)(temp * 16) >> 8;
+			uint8_t lo = (uint16_t)(temp * 16);
+			uint16_t dauer = boost::lexical_cast<int>(matches[3]) * 60; // Sekunden!
+			m_tcon.send_HEIZUNG_SET_MODE_THERMOSTAT_DETAILS(m_src,
+					HCAN_MULTICAST_CONTROL,id,hi,lo,dauer>>8,dauer);
+			return true;
+		}
 	}
-
-	boost::regex re2("^heizung *([[:word:]-]+) *$");
-	if (boost::regex_match(line.c_str(), matches, re2))
+	catch (traceable_error &e)
 	{
-		print_header();
-		print_heizung(global_installation_data.heizung_id_by_name(
-					matches[1]));
-		return true;
-	}
-
-	boost::regex re3("^heizung *([[:word:]-]+) +auto *$");
-	if (boost::regex_match(line.c_str(), matches, re3))
-	{
-		uint8_t id = global_installation_data.heizung_id_by_name(
-				matches[1]);
-		m_tcon.send_HEIZUNG_SET_MODE_AUTOMATIK(m_src, 
-				HCAN_MULTICAST_CONTROL,id);
-		return true;
-	}
-
-	boost::regex re4("^heizung *([[:word:]-]+) +aus *$");
-	if (boost::regex_match(line.c_str(), matches, re4))
-	{
-		uint8_t id = global_installation_data.heizung_id_by_name(
-				matches[1]);
-		m_tcon.send_HEIZUNG_SET_MODE_OFF(m_src, 
-				HCAN_MULTICAST_CONTROL,id);
-		return true;
-	}
-
-	boost::regex re5("^heizung *([[:word:]-]+) +manuell +([0-9]+) +([0-9]+) *$");
-	if (boost::regex_match(line.c_str(), matches, re5))
-	{
-		uint8_t id = global_installation_data.heizung_id_by_name(
-				matches[1]);
-		uint8_t stellgrad = boost::lexical_cast<int>(matches[2]);
-		uint16_t dauer = boost::lexical_cast<int>(matches[3]) * 60; // Sekunden!
-		m_tcon.send_HEIZUNG_SET_MODE_MANUAL(m_src, 
-				HCAN_MULTICAST_CONTROL,id,stellgrad,dauer>>8,dauer);
-		return true;
-	}
-
-	boost::regex re6("^heizung *([[:word:]-]+) +therm +([0-9\\.]+) +([0-9]+) *$");
-	if (boost::regex_match(line.c_str(), matches, re6))
-	{
-		uint8_t id = global_installation_data.heizung_id_by_name(
-				matches[1]);
-		float temp = boost::lexical_cast<float>(matches[2]);
-		uint8_t hi = (uint16_t)(temp * 16) >> 8;
-		uint8_t lo = (uint16_t)(temp * 16);
-		uint16_t dauer = boost::lexical_cast<int>(matches[3]) * 60; // Sekunden!
-		m_tcon.send_HEIZUNG_SET_MODE_THERMOSTAT_DETAILS(m_src, 
-				HCAN_MULTICAST_CONTROL,id,hi,lo,dauer>>8,dauer);
-		return true;
+		cerr << e.what() << endl;
 	}
 
 	return false;
